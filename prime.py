828bcdb31b986e893c339e7e6de5dc5ba6d56060
GoldOliverK09 <oliver.klumpler@outlook.com>
2026-09-12 11:42:18 +0100

    optimised primes

--- prime.py
+++ prime.py
@@ -1,18 +1,41 @@
-primes = []
+import math
+from numba import jit
 
-def is_prime(num):
-    if num < 2: #checks if a number is prime
+
+@jit(cache=True)
+def is_prime(num: int) -> bool:
+    if num < 2:  # checks if a number is prime
         return False
 
-    for i in range(2, num):
+    for i in range(
+        2, int(math.sqrt(num)) + 1
+    ):  # checks if a number is divisible by any number up to its square root
         if num % i == 0:
             return False
 
     return True
 
-for num in range (2, 10000): #range of prime numbers
-    if is_prime(num):
-        primes.append(num)
 
-#print(primes) #the list of primes
+def generate_spiral(size):
 
+    x, y = 0, 0
+    coordinates = [(x, y)]
+
+    return coordinates
+
+if __name__ == "__main__":
+    primes: set[int] = set()
+    for n in range(2, 100000):  # range of prime numbers
+        if is_prime(n):
+            primes.add(n)
+
+    print(primes)  # the list of primes
+
+    directions = [
+        (1, 0),
+        (0, 1),
+        (-1, 0),
+        (0, -1),
+        ]
+
+    print(generate_spiral(25)) 
